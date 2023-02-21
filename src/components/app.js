509a1449bd34e94336c20015f8907760e3bc0907
KathrinN <katrinnn757@gmail.com>
2023-02-21 23:58:17 +0500

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./app.scss";
+
+const App = () => {
+  return (
+    <div className="full-screen">
+      <div>
+        <h1>
+          React with webpack
+        </h1>
+        <a
+          className="button-line"
+          href="https://github.com/katrinkattt"
+          target="_blank"
+        >
+          info
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
